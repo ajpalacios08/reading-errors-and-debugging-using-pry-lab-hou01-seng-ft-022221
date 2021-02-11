c6f49ce67851cc9c41e14350eda10d1045b09425
Allen Palacios <apalacios1994@gmail.com>
2021-02-11 04:46:17 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,8 +2,7 @@
 require 'pry'
 
 def start_game(player1, player2)
-  greetings = "Hello #{player1} & #{playar2}"
-  greetings
+  "Hello #{player1} & #{player2}"
 end
 
 def play_game(player1, player2)
